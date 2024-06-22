145a3fb16d96636191a24ffe6c1da6fa9050f75b
haidarim <mehdihaidari.main@gmail.com>
2024-06-22 02:28:44 +0200

    added .env loader class

--- back-end/er-to-backend/ets-backend/src/main/java/com/ets/ets_backend/util/EnvVarLoader.java
+++ back-end/er-to-backend/ets-backend/src/main/java/com/ets/ets_backend/util/EnvVarLoader.java
@@ -1,4 +1,18 @@
 package com.ets.ets_backend.util;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.stereotype.Component;
+
+@Component
 public class EnvVarLoader {
+
+    private static final Dotenv dotenv = Dotenv.configure().directory("../../postgres-container").load();
+
+    public static void loadEnvVars() {
+        System.setProperty("POSTGRES_HOST", dotenv.get("POSTGRES_HOST"));
+        System.setProperty("POSTGRES_PORT", dotenv.get("POSTGRES_PORT"));
+        System.setProperty("POSTGRES_DB", dotenv.get("POSTGRES_DB"));
+        System.setProperty("POSTGRES_USER", dotenv.get("POSTGRES_USER"));
+        System.setProperty("POSTGRES_PASSWORD", dotenv.get("POSTGRES_PASSWORD"));
+    }
 }
