685091fbb27ecda628a3f3d245ab350cde20d1c8
haidarim <mehdihaidari.main@gmail.com>
2024-06-22 22:40:01 +0200

    added configs to allow CORS

--- back-end/er-to-backend/ets-backend/src/main/java/com/ets/ets_backend/config/WebConfig.java
+++ back-end/er-to-backend/ets-backend/src/main/java/com/ets/ets_backend/config/WebConfig.java
@@ -1,4 +1,25 @@
 package com.ets.ets_backend.config;
 
-public class WebConfig {
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * To enable Cross-Origin Resource Sharing (CORS) on the Spring Boot backend.
+ * CORS allows the server to specify who can access its resources.
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        //  addMapping("/**"): Defines which endpoints (URL patterns) CORS should be applied to.
+        //  In this case, /** means apply CORS to all endpoints.
+
+
+        registry.addMapping("/**")
+                .allowedOrigins("*") // allow all origins (domains)
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Allowed methods
+                .allowedHeaders("*");
+    }
 }
