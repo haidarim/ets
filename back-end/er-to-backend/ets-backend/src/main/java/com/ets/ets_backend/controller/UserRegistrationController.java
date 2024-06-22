102b19842b6dda9b70bbc1c2d8f21fb95d82df11
haidarim <mehdihaidari.main@gmail.com>
2024-06-22 22:37:07 +0200

    added sign-up post controller

--- back-end/er-to-backend/ets-backend/src/main/java/com/ets/ets_backend/controller/UserRegistrationController.java
+++ back-end/er-to-backend/ets-backend/src/main/java/com/ets/ets_backend/controller/UserRegistrationController.java
@@ -1,7 +1,24 @@
 package com.ets.ets_backend.controller;
 
 
+import com.ets.ets_backend.model.User;
+import com.ets.ets_backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping(path = "api0/sign-up")
 public class UserRegistrationController {
 
+    @Autowired
+    private UserService service;
+
+    @PostMapping
+    public ResponseEntity<Void> createUser(@RequestBody User user){
+        service.createUser(user);
+        // set the status of the response and build it without body.
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
 }
