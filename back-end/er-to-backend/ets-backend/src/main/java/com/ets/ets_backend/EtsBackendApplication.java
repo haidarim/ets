42492dae9050985fd01cbca06ffc1b4e6f62946b
haidarim <mehdihaidari.main@gmail.com>
2024-06-22 22:37:35 +0200

    ...

--- back-end/er-to-backend/ets-backend/src/main/java/com/ets/ets_backend/EtsBackendApplication.java
+++ back-end/er-to-backend/ets-backend/src/main/java/com/ets/ets_backend/EtsBackendApplication.java
@@ -3,6 +3,7 @@ package com.ets.ets_backend;
 import com.ets.ets_backend.util.EnvVarLoader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 /**
