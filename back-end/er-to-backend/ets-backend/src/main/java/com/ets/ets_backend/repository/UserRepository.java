b61a7a6aa960d4fd01fb0eb1a0e941c839253f17
haidarim <mehdihaidari.main@gmail.com>
2024-07-10 18:56:06 +0200

    ...

--- back-end/er-to-backend/ets-backend/src/main/java/com/ets/ets_backend/repository/UserRepository.java
+++ back-end/er-to-backend/ets-backend/src/main/java/com/ets/ets_backend/repository/UserRepository.java
@@ -2,10 +2,21 @@ package com.ets.ets_backend.repository;
 
 import com.ets.ets_backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
     // sophisticated queries if needed! else enough :)
+
+    // @Query("update clients set clients.password = :pass where clients.Id = :Id and clients.email = :email and clients.username = :username")
+    // Optional<Void> changeUserPassword(@Param("Id") Long Id, @Param("password") String password, @Param("email") String email, @Param("username") String username);
+
+
+  //  @Query("SELECT u FROM User u WHERE u.email = :email")
+  // List<User> findByEmail(@Param("email") String email);
+
 }
