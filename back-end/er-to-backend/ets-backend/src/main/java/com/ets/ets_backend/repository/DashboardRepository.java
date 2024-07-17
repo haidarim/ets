7e1ae908a8f9dc8de14ba4ee39f4104d718fa3a1
haidarim <mehdihaidari.main@gmail.com>
2024-07-18 01:10:16 +0200

    created dashboard repository

--- back-end/er-to-backend/ets-backend/src/main/java/com/ets/ets_backend/repository/DashboardRepository.java
+++ back-end/er-to-backend/ets-backend/src/main/java/com/ets/ets_backend/repository/DashboardRepository.java
@@ -1,2 +1,23 @@
-package com.ets.ets_backend.repository;public interface DashboardRepository {
+package com.ets.ets_backend.repository;
+
+import com.ets.ets_backend.model.Dashboard;
+import com.ets.ets_backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface DashboardRepository extends JpaRepository<User, Long> {
+    Optional<Dashboard> findByUsername(String username);
+    // sophisticated queries if needed! else enough :)
+
+    // @Query("update clients set clients.password = :pass where clients.Id = :Id and clients.email = :email and clients.username = :username")
+    // Optional<Void> changeUserPassword(@Param("Id") Long Id, @Param("password") String password, @Param("email") String email, @Param("username") String username);
+
+
+    //  @Query("SELECT u FROM User u WHERE u.email = :email")
+    // List<User> findByEmail(@Param("email") String email);
+
 }
