71f80f90883cb9f20c49be890b3d2bea57b544fa
haidarim <mehdihaidari.main@gmail.com>
2024-08-22 01:19:09 +0200

    ...

--- front-end/er-to-sql/src/component/Project.js
+++ front-end/er-to-sql/src/component/Project.js
@@ -1,24 +1,77 @@
-import {useRef, useState, useEffect} from 'react';
+import React, { useRef, useState } from 'react';
 
 function Project() {
-    const canvasRef = useRef(null); // ref of the canvas to be set by useEffect
-    const [scale, setScale] = useState(1); // scale of the marked area
-    const handleMouseDown = (e)=>{
-    
-    }
+    const canvasRef = useRef(null);
+    const [x0, setX0] = useState(0);
+    const [y0, setY0] = useState(0);
+    const [dragging, setDragging] = useState(false);
+
+    const handleMouseDown = (e) => {
+        setDragging(true);
+        const canvas = canvasRef.current;
+        if (canvas) {
+            setX0(e.nativeEvent.offsetX);
+            setY0(e.nativeEvent.offsetY);
+        }
+    };
+
+    const handleMouseMove = (e) => {
+        if (dragging) {
+            const canvas = canvasRef.current;
+            if (canvas) {
+                const ctx = canvas.getContext("2d");
+
+                // Clear the canvas before redrawing
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+                const x = e.nativeEvent.offsetX;
+                const y = e.nativeEvent.offsetY;
+
+                const width = x - x0;
+                const height = y - y0;
+
+                ctx.strokeStyle = `rgb(${0}, ${102}, ${25})`;
+                ctx.beginPath();
+                ctx.rect(x0, y0, width, height);
+                ctx.stroke();
+            }
+        }
+    };
+
+    const handleMouseUp = () => {
+        setDragging(false);
+    };
 
     return (
         <div className="project_container">
             <nav className="project_tools_nav">
                 {/* TODO: add tools like save, gen-button, ...*/}
+                <ul className='list tool'>
+                    <li></li>
+                </ul>
+                <button>To SQL</button>
+                <div className='entity tool'></div>
+                <div className='relationship tool'></div>
+                <div className='attribute tool'></div>
+                <div className='line tool'></div>
             </nav>
             <main className="project_main">
                 <section className="er_tools_section">
                     {/* TODO: ER tools here, entity, relation , ... */}
                 </section>
                 <section className="canvas_container">
-                    <div className="drawing_area">
-                        <canvas ref="canvasRef" onMouseDown={handleMouseDown} className="project_canvas"></canvas>
+                    <div className="drawing_area"
+                        onMouseMove={handleMouseMove}
+                        onMouseUp={handleMouseUp}
+                        onMouseDown={handleMouseDown}
+                    >
+                        <canvas
+                            ref={canvasRef}
+                            className="project_canvas"
+                            width={800} // Set your desired canvas width
+                            height={600} // Set your desired canvas height
+                        >
+                        </canvas>
                     </div>
                 </section>
             </main>
